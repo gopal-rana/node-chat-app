a810421655573861c7c7033e65b2fc7ca28b9330
gopal <gopal.singh@niit-tech.com>
2018-08-16 03:44:35 -0400

    Add messages and show in browser

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,28 @@ socket.on('connect', function(){
 
 socket.on('newMessage', function(message){
   console.log('new message', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  $('#messages').append(li);
 });
 
 socket.on('disconnect', function(){
   console.log('Disconnected from server');
+});
+
+socket.emit('createMessage', {
+  from: 'john',
+  text: 'Hi'
+}, function(data){
+  console.log('Got it', data)
+});
+
+$('#message-form').on('submit', function(e){
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function(data){
+    console.log('Got it', data)
+  });
 });
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publichPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3100;
 var app = express();
@@ -14,25 +15,14 @@ app.use(express.static(publichPath));
 io.on('connection', (socket) =>{
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New User Joined',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
 
-  socket.on('createMessage', function(message){
+  socket.on('createMessage', (message, callback) =>{
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
